cde4d4d72a2a5b2de08a65b6d3246d44226c99f2
Norbert305 <sealsnorbert@yahoo.com>
2022-01-13 00:17:52 +0000

    color generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const colors = ["green", "red", "pink", "blue", "yellow", "orange", "purple"];
+  const btn = document.getElementById("btn");
+  const color = document.querySelector(".color");
+
+  btn.addEventListener("click", function() {
+    const randomNumber = getRandomNumber();
+    // console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+  });
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+  }
 };
