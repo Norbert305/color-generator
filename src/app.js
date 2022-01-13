8046a26a5f2d3d7896f8fa9ff7a91256ff8ebea5
Norbert305 <sealsnorbert@yahoo.com>
2022-01-13 00:40:15 +0000

    color-generator

--- src/app.js
+++ src/app.js
@@ -6,19 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const colors = ["green", "red", "pink", "blue", "yellow", "orange", "purple"];
-  const btn = document.getElementById("btn");
-  const color = document.querySelector(".color");
+  const colors = ["green", "red", "pink", "blue", "yellow", "orange", "purple"]; //my array of colors
+  const btn = document.getElementById("btn"); //id button from my index.html page
+  const color = document.querySelector(".color"); // class color from my index.html page
 
   btn.addEventListener("click", function() {
     const randomNumber = getRandomNumber();
-    // console.log(randomNumber);
+    // applying getRandomNumber() to the randomNumber variable
 
-    document.body.style.backgroundColor = colors[randomNumber];
+    document.body.style.backgroundColor = colors[randomNumber]; //colors[random length of my colors array];
     color.textContent = colors[randomNumber];
+    //decorating the background of the entire body web-page to the random colors of the array.
+    //also applying the random colors of the text to change when clicking the button.
   });
 
   function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
+    //Math.floor rounds the number down to a whole number
+    //Math.random creates a random decimal number between 0 and 1.
+    //Basically using this function to generate random numbers equal to the length of my colors array.
   }
 };
